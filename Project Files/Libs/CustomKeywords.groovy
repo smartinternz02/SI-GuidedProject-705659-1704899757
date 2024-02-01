e466952ff8cd2e846640211d30c281ce4b83956e
sowjanya2002 <64244804+sowjanya2002@users.noreply.github.com>
2024-02-01 13:53:40 +0530

    Add files via upload

---
+++ Project Files/Libs/CustomKeywords.groovy
@@ -0,0 +1,30 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+import com.kms.katalon.core.testobject.TestObject
+
+
+
+def static "com.example.WebUICustomKeywords.printHello"() {
+    (new com.example.WebUICustomKeywords()).printHello()
+}
+
+
+def static "com.example.WebUICustomKeywords.printName"(
+    	String name	) {
+    (new com.example.WebUICustomKeywords()).printName(
+        	name)
+}
+
+
+def static "com.example.WebUICustomKeywords.CheckDropDownListElementExist"(
+    	TestObject object	
+     , 	String option	) {
+    (new com.example.WebUICustomKeywords()).CheckDropDownListElementExist(
+        	object
+         , 	option)
+}
